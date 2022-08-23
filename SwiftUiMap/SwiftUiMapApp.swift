bbf510d7cc1c7d14d60458c12ed2e71548002b2f
Luiz <diegoo837ma@icloud.com>
2022-08-23 21:11:55 +0100

    Initial Commit

---
+++ SwiftUiMap/SwiftUiMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiMapApp.swift
+//  SwiftUiMap
+//
+//  Created by Luiz Andrade on 23/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
